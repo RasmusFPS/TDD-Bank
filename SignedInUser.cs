bec3b2892b93e7db59b81120d19b9e7d8f7b7b22
RasmusFPS <Rasmus.andersen05@gmail.com>
2025-11-17 15:40:54 +0100

    Deposite method added

--- SignedInUser.cs
+++ SignedInUser.cs
@@ -8,5 +8,12 @@ namespace TDD_Bank
 {
     internal class SignedInUser
     {
+        double Balance;
+        public void Deposite()
+        {
+            Console.WriteLine("How much money do you want to Deposite!");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Balance += input;
+        }
     }
 }
