6166e7c64f33677fa16cdf16e16dee10e35205d8
LouiseBGit <lowalis91@gmail.com>
2025-11-19 10:39:55 +0100

    Added properties in Data

--- Data.cs
+++ Data.cs
@@ -8,5 +8,13 @@ namespace TDD_Bank
 {
     internal class Data
     {
+        public static List<User> UserCollection = new List<User>()
+        {
+            new User( "Admin-Johan", "1234", true),
+            new User( "Carl", "Hawaa", false)
+
+        };
+
+        public static List<Exchange> Currency = new();
     }
 }
