f464abec28d1adb7b5bc166e8d0f6c0f6b802056
fannybillefalt <fannybillefalt@gmail.com>
2025-11-25 09:15:30 +0100

    Reused in a method from echange class

--- BankTransfer.cs
+++ BankTransfer.cs
@@ -50,13 +50,19 @@
                 return false;
             }
 
+            //if (fromAccount.Currency != toAccount.Currency)
+            //{
+            //    Console.WriteLine("The accounts have different currencies, transfer not possible.");
+            //    return false;
+            //}
+
             if (fromAccount.Currency != toAccount.Currency)
             {
-                Console.WriteLine("The accounts have different currencies, transfer not possible.");
-                return false;
+                Exchange exchange = new Exchange();
+                exchange.ExchangeStart(toAccount);
             }
 
-            Console.WriteLine($"How much do you want to transfer? Saldo: {fromAccount.Balance} {fromAccount.Currency}");
+                Console.WriteLine($"How much do you want to transfer? Saldo: {fromAccount.Balance} {fromAccount.Currency}");
             if (!decimal.TryParse(Console.ReadLine(), out decimal amount) || amount <= 0)
             {
                 Console.WriteLine("Invalid amount.");
