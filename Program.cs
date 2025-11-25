e17643547a454c90976cb7b15a7067c89853512d
RasmusFPS <Rasmus.andersen05@gmail.com>
2025-11-25 12:18:01 +0100

    Also changed main

--- Program.cs
+++ Program.cs
@@ -8,19 +8,6 @@ namespace TDD_Bank
         static void Main(string[] args)
         {
             TDDBank bank = new TDDBank();
-            User loggedInUser = TDDBank.SignIn();
-
-            if (loggedInUser is Client currentClient)
-            {
-                RunClientDashboard(currentClient);
-            }
-            else if (loggedInUser is Admin admin)
-            {
-                UI.AdminMenu();
-
-            }
-
-            Console.WriteLine("Thank you for using TDD Bank");
 
             bank.Run();
         }
